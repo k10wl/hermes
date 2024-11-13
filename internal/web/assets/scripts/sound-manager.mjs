9b0334a5184d9e5674a9caa8e74a9a3bf4231d88
k10wl <aleks.antonsheko@gmail.com>
2024-11-13 20:48:38 +0200

    feat: add sound management functionality

---
+++ internal/web/assets/scripts/sound-manager.mjs
@@ -0,0 +1,33 @@
+export class SoundManager {
+  /** @typedef {keyof typeof SoundManager['availableSounds']} SoundName */
+
+  static #path = "/assets/sounds/";
+  static availableSounds = {
+    "message-in": "message-in.mp3",
+  };
+
+  /** @param {SoundName} name */
+  static play(name) {
+    const src = SoundManager.#getSrc(name);
+    const audio = document.createElement("audio");
+    SoundManager.#setupAudioTeardown(audio);
+    audio.src = src;
+    audio.play();
+  }
+
+  /** @param {SoundName} name */
+  static #getSrc(name) {
+    const filename = SoundManager.availableSounds[name];
+    if (!filename) {
+      throw new Error(`unknown audio "${name}"`);
+    }
+    return this.#path + filename;
+  }
+
+  /** @param {HTMLAudioElement} audio  */
+  static #setupAudioTeardown(audio) {
+    const teardown = () => audio.remove();
+    audio.onended = teardown;
+    audio.onerror = teardown;
+  }
+}
