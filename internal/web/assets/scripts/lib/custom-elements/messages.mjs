007107efee1ff0f4e61431660585619115fbd3da
k10wl <aleks.antonsheko@gmail.com>
2025-03-11 00:33:22 +0200

    style: update messages ui

--- internal/web/assets/scripts/lib/custom-elements/messages.mjs
+++ internal/web/assets/scripts/lib/custom-elements/messages.mjs
@@ -1,28 +1,105 @@
+import { AssertInstance } from "/assets/scripts/lib/assert.mjs";
 import { RequestReadChatEvent } from "/assets/scripts/lib/events/client-events-list.mjs";
 import { ServerEvents } from "/assets/scripts/lib/events/server-events.mjs";
+import { html } from "/assets/scripts/lib/html-v2.mjs";
 import { LocationControll } from "/assets/scripts/lib/location-control.mjs";
 
 export class Messages extends HTMLElement {
   /** @type {(() => void)[]} */
   #cleanupOnDisconnect = [];
 
+  /** @type {HTMLElement | undefined} */
+  #messages;
+
   constructor() {
     super();
+    this.shadow = this.attachShadow({ mode: "open" });
+    this.shadow.append(html`
+      <style>
+        :host {
+          --_text: var(--text-0);
+          --_primary: var(--primary);
+        }
+
+        .message {
+          margin: 0.75rem 0;
+        }
+
+        .content {
+          user-select: text;
+          white-space: pre-wrap;
+          word-break: break-word;
+        }
+
+        .role-user {
+          --_border-radius: 0.66rem;
+          color: var(--_text);
+          background: rgb(from var(--_primary) r g b / 0.05);
+          border: 1px solid rgb(from var(--_primary) r g b / 0.25);
+          margin-left: auto;
+          width: fit-content;
+          max-width: 80%;
+          border-radius: var(--_border-radius) var(--_border-radius) 0
+            var(--_border-radius);
+          padding: 0.33rem;
+          padding-bottom: 0.2rem;
+        }
+
+        .actions {
+          display: flex;
+          gap: 0.5rem;
+          margin-top: 0.15rem;
+        }
+
+        .actions button {
+          background: var(--bg-2);
+          border: 1px solid rgb(from var(--text-0) r g b / 0.25);
+          font-size: 1rem;
+          --_size: 2ch;
+          width: var(--_size);
+          height: var(--_size);
+          border-radius: calc(var(--_size) / 4);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          padding: 0.25rem;
+          color: rgb(from var(--text-0) r g b / 0.5);
+          background: transparent;
+          border: 1px solid transparent;
+          &:hover {
+            color: var(--_text);
+            border-color: rgb(from var(--text-0) r g b / 0.25);
+          }
+        }
+      </style>
+
+      <section
+        bind="${(/** @type {unknown} */ el) => {
+          this.#messages = AssertInstance.once(el, HTMLElement);
+        }}"
+      ></section>
+    `);
   }
 
   connectedCallback() {
-    const container = document.createElement("div");
-    this.appendChild(container);
-    const messagesViewObserver = new MessagesViewObserver(container);
-    const routeObserver = new RouteObserver(container);
+    const messagesContainer = AssertInstance.once(this.#messages, HTMLElement);
+    const routeObserver = new RouteObserver();
     routeObserver.notify();
-    const messageCreatedObserver = new MessageCreatedObserver(container);
     this.#cleanupOnDisconnect.push(
       LocationControll.attach(routeObserver),
       ServerEvents.on("message-created", (data) => {
-        messageCreatedObserver.notify(data);
+        if (data.payload.chat_id === LocationControll.chatId) {
+          messagesContainer.append(this.#messageToHtml(data.payload.message));
+        }
       }),
-      ServerEvents.on("read-chat", (data) => messagesViewObserver.notify(data)),
+      ServerEvents.on("read-chat", (data) => {
+        console.log("read-chat", data);
+        messagesContainer.replaceChildren(
+          ...data.payload.messages.map((message) =>
+            this.#messageToHtml(message),
+          ),
+        );
+      }),
     );
   }
 
@@ -31,76 +108,59 @@ export class Messages extends HTMLElement {
       cleanup();
     }
   }
+
+  /** @param {import("/assets/scripts/models.mjs").Message} message */
+  #messageToHtml(message) {
+    const escaped = message.content
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;");
+
+    if (message.role === "user") {
+      return html`
+        <div class="message content role-${message.role}">${escaped}</div>
+      `;
+    }
+
+    return html`
+      <div class="message">
+        <div class="content">${escaped}</div>
+        <div class="actions">
+          <button
+            onclick="${(/** @type {Event} */ event) => {
+              const button = AssertInstance.once(
+                event.target,
+                HTMLButtonElement,
+              );
+              const prev = button.textContent;
+              button.textContent = "✓";
+              setTimeout(() => {
+                button.textContent = prev;
+              }, 2000);
+              window.navigator.clipboard.writeText(message.content);
+            }}"
+          >
+            ⎘
+          </button>
+        </div>
+      </div>
+    `;
+  }
 }
 
 class RouteObserver {
-  container;
-
   /** @type {number | null} */
   #last = null;
 
-  /** @param {HTMLElement} container  */
-  constructor(container) {
-    this.container = container;
-  }
+  constructor() {}
 
   notify() {
     const chatId = LocationControll.chatId;
     const prev = this.#last;
     this.#last = chatId;
-    if (prev === chatId) {
+    if (prev === chatId || !chatId) {
       return;
     }
-    if (chatId) {
-      ServerEvents.send(new RequestReadChatEvent(chatId));
-      return;
-    }
-    this.container.innerHTML = "";
-  }
-}
-
-class MessagesViewObserver {
-  container;
-  /** @param {HTMLElement} container  */
-  constructor(container) {
-    this.container = container;
-  }
-
-  /** @param {import( "../events/server-events-list.mjs").ReadChatEvent} readChatEvent  */
-  notify(readChatEvent) {
-    this.container.innerHTML = "";
-    this.container.append(
-      ...readChatEvent.payload.messages.map((message) =>
-        MessageCreator.createElement(message),
-      ),
-    );
-  }
-}
-
-class MessageCreatedObserver {
-  #container;
-  /** @param {HTMLElement} container  */
-  constructor(container) {
-    this.#container = container;
-  }
-  /** @param {import("../events/server-events-list.mjs").MessageCreatedEvent } event  */
-  notify(event) {
-    if (event.payload.chat_id !== LocationControll.chatId) {
-      return;
-    }
-    this.#container.append(MessageCreator.createElement(event.payload.message));
-  }
-}
-
-class MessageCreator {
-  /** @param {import("/assets/scripts/models.mjs").Message} message  */
-  static createElement(message) {
-    const div = document.createElement("div");
-    div.classList.add("message", `role-${message.role}`, "selectable");
-    div.id = `message-${message.id}`;
-    const pre = document.createElement("pre");
-    pre.innerText = message.content.trim();
-    div.appendChild(pre);
-    return div;
+    ServerEvents.send(new RequestReadChatEvent(chatId));
   }
 }
