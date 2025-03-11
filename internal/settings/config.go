dea17f56b844499f63f4b174761373e083029c3c
k10wl <aleks.antonsheko@gmail.com>
2025-03-11 23:27:43 +0200

    chore: bumb version

--- internal/settings/config.go
+++ internal/settings/config.go
@@ -9,8 +9,8 @@ import (
 	"sync"
 )
 
-const Version = "5.3.1"
-const VersionDate = "2025-03-03"
+const Version = "5.4.0 (dev)"
+const VersionDate = "2025-03-11"
 
 const DefaultDatabaseName = "main.db"
 
